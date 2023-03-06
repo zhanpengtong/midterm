6e2167be5726ecaff00651b58d9efabef7806b07
Zhanpeng Tong <zhanpeng@login-students.khoury.northeastern.edu>
2023-03-06 03:56:15 -0500

    updata

--- Fibonacci_series.c
+++ Fibonacci_series.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <time.h>
 
-int fibonacci_iterative(int n) {
+long fibonacci_iterative(int n) {
     if (n == 0) {
         return 0;
     } else if (n == 1) {
         return 1;
     } else {
-        int a = 0, b = 1, c;
+        long a = 0, b = 1, c;
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
@@ -16,7 +17,7 @@ int fibonacci_iterative(int n) {
     }
 }
 
-int fibonacci_recursive(int n) {
+long fibonacci_recursive(int n) {
     if (n == 0) {
         return 0;
     } else if (n == 1) {
@@ -26,8 +27,8 @@ int fibonacci_recursive(int n) {
     }
 }
 
-int fibonacci_dynamic(int n) {
-    int fib[n+1];
+long fibonacci_dynamic(int n) {
+    long fib[n+1];
     fib[0] = 0;
     fib[1] = 1;
     for (int i = 2; i <= n; i++) {
@@ -37,9 +38,43 @@ int fibonacci_dynamic(int n) {
 }
 
 int main() {
-    int n = 10;
-    printf("The %dth Fibonacci number using iterative approach is: %d\n", n, fibonacci_iterative(n));
-    printf("The %dth Fibonacci number using recursive approach is: %d\n", n, fibonacci_recursive(n));
-    printf("The %dth Fibonacci number using dynamic programming approach is: %d\n", n, fibonacci_dynamic(n));
+    int n = 40;
+
+    FILE *file = fopen("fibonacci_running_times_c.txt", "w");
+    fprintf(file, "Running time table for Fibonacci series calculation\n\n");
+
+    // Recursive approach
+    fprintf(file, "Calculating the running time for the first %d Fibonacci numbers using recursive approach...\n", n+1);
+    for (int i = 0; i <= n; i++) {
+        clock_t start_time = clock();
+        long result = fibonacci_recursive(i);
+        clock_t end_time = clock();
+        double running_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+        fprintf(file, "n = %d, Result = %ld, Running time = %f seconds\n", i, result, running_time);
+    }
+    fprintf(file, "\n");
+
+    // Iterative approach
+    fprintf(file, "Calculating the running time for the first %d Fibonacci numbers using iterative approach...\n", n+1);
+    for (int i = 0; i <= n; i++) {
+        clock_t start_time = clock();
+        long result = fibonacci_iterative(i);
+        clock_t end_time = clock();
+        double running_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+        fprintf(file, "n = %d, Result = %ld, Running time = %f seconds\n", i, result, running_time);
+    }
+    fprintf(file, "\n");
+
+    // Dynamic programming approach
+    fprintf(file, "Calculating the running time for the first %d Fibonacci numbers using dynamic programming approach...\n", n+1);
+    for (int i = 0; i <= n; i++) {
+        clock_t start_time = clock();
+        long result = fibonacci_dynamic(i);
+        clock_t end_time = clock();
+        double running_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+        fprintf(file, "n = %d, Result = %ld, Running time = %f seconds\n", i, result, running_time);
+    }
+
+    fclose(file);
     return 0;
 }
